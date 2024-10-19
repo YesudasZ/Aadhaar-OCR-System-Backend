4e0373160136271f1dd33f4112843369cb516e41
YesudasZ <yesudasmj50@gmail.com>
2024-10-19 12:24:46 +0530

    Backend Hosted

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:process.env.CLIENT_URL,
   methods: [ "POST"],
   credentials: true,
 }));
